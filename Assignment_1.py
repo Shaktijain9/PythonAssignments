e854c9207164a345c2966357c54b5e26803c5a47
Shakti Jain <42385445+Shaktijain9@users.noreply.github.com>
2022-08-02 21:19:18 +0530

    Uploading Assignment 1

---
+++ Assignment_1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  1. expressions: *, /, +, -
+# values : 6, 'hello', -87.8
+# 
+
+# 2. String is a collection of characters i.e., a kind of information whereas variable is a place holder to store an information,
+# Example - Variable is more like a place or address where a particular house is while string can be considered as a type of house.
+
+# 3. Datatypes in python are of two types:
+# a) Mutable data types :  list, dict, set 
+# b) Immutable data types :  int, float, bool, string, unicode, tuple.
+
+# 4. Expressions are made up of operators and operands, e.g., 2+3 where 2 & 3 are operands and "+" is an operator(binary).Expressions tends to perform some operations and results into some value.
+
+# 5. Expressions basically performs metioned oeprations and returns some value whereas statements are standalone unit of programming languages which consists of expressions and other components.
+
+# In[2]:
+
+
+#6
+
+bacon  = 22
+bacon+1
+#bacon will still contain 22 since there is no assignment to bacon varibale after increament
+
+
+# In[3]:
+
+
+#7 a)
+'spam' + 'spamspam'
+
+
+# In[4]:
+
+
+#7 b)
+'spam' * 3
+
+
+# 8. As per the naming convention of variables in python , a varible can be a string of characters but can not start with number.
+# Hence, egg is a valid name whereas 100 is not.
+
+# 9. int(), float(), str() 
+
+# 10. 'I have eaten ' + 99 + ' burritos.' will cause an error because string and int addition is not allowed in python.
+# In order to resolve the issue we can convert 99 into string
+
+# In[6]:
+
+
+'I have eaten ' + str(99)+ ' burritos.'
+
+
+# In[ ]:
+
+
+
+
