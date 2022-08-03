3b31a487d6eadd05f84c55910d9e99afe8cd1c83
Shakti Jain <42385445+Shaktijain9@users.noreply.github.com>
2022-08-03 21:11:58 +0530

    Uploading Programming Assignment 1

---
+++ Programming_Assingment1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1
+
+print("HelloWorld")
+
+
+# In[9]:
+
+
+#2
+a, b = list(map(int, input("Enter two numbers: ").split()))
+print("Addition: ", a+b)
+print("Division: ", a/b)
+
+
+# In[8]:
+
+
+#3
+base, height  = list(map(int, input("Enter base and height of triangle: ").split()))
+print("Area of triangle : ", 1/2*base*height)
+
+
+# In[10]:
+
+
+#4
+a, b = list(map(int, input("Enter value of a and vlue of b: ").split()))
+
+a, b = b, a
+print(f"Value of a = {a} & b={b}")
+
+
+# In[14]:
+
+
+#5
+import random
+#Any value between 0 & 5
+print(random.randint(0, 5))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
